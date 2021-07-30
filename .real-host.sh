8ac66f92df2c433b1e789b6dc787ca1b04e869cd
Roslandas <59371841+Roslandas@users.noreply.github.com>
2021-07-30 22:34:41 +0300

    Update .real-host.sh

--- .real-host.sh
+++ .real-host.sh
@@ -106,6 +106,11 @@ echo -e "\e[1;36mNunca dejes de aprender... :)\e[0m"
 echo ""
 read foo;
 ;;
+97)clear
+echo "translating to lithuania language...";
+sleep 3
+bash real-host-v3.sh
+;;
 98)clear
 echo "Traduciendo al idioma ingles...";
 sleep 4
